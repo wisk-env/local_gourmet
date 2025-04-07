07dda21aa29f5d450833053174a6ee956417729a
wisk-env <wiskenv566@gmail.com>
2025-04-07 14:03:22 +0900

    Google Maps API keyを環境変数に設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
+  resources :restaurants
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
