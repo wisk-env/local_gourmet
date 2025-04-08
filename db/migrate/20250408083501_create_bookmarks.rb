5065364bce18e565bb2281cdb1fe821a2c4335b1
wisk-env <wiskenv566@gmail.com>
2025-04-09 00:51:58 +0900

    Bookmarkモデルの作成

---
+++ db/migrate/20250408083501_create_bookmarks.rb
@@ -0,0 +1,11 @@
+class CreateBookmarks < ActiveRecord::Migration[7.1]
+  def change
+    create_table :bookmarks do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :restaurant, null: false, foreign_key: true
+
+      t.timestamps
+      t.index [:user_id, :restaurant_id], unique: true
+    end
+  end
+end
