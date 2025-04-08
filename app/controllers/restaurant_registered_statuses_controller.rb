c07c8dd7e11f804e2e6559b7b4a72615b4a74015
wisk-env <wiskenv566@gmail.com>
2025-04-08 14:40:30 +0900

    口コミを投稿したいお店がrestaurantsテーブルに保存されているか判定する処理を行うコントローラーを作成

---
+++ app/controllers/restaurant_registered_statuses_controller.rb
@@ -0,0 +1,24 @@
+class RestaurantRegisteredStatusesController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.find_or_initialize_by(check_params)
+    if @restaurant && @restaurant.name
+      redirect_to restaurant_registered_status_path(@restaurant)
+    else
+      redirect_to restaurant_unregistered_statuses_path(check_params)
+    end
+  end
+
+  private
+
+  def check_params
+    params.permit(:lat, :lng, :address)
+  end
+end
