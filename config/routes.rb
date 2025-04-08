7337b21afe60cbadd522c77a27252d7240399675
wisk-env <wiskenv566@gmail.com>
2025-04-09 00:54:21 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,14 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
-  resources :restaurants
+  resources :restaurants do
+    collection do
+      get :bookmarks
+    end
+  end
   resources :restaurant_registered_statuses, only: %i[index show new]
   resources :restaurant_unregistered_statuses, only: %i[index]
+  resources :bookmarks, only: %i[create destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
