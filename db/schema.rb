8ed32ddf775ce6d84c4f20e19e9500b685471bcf
wisk-env <wiskenv566@gmail.com>
2025-04-08 14:41:26 +0900

    restaurantsテーブルにaddressカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_04_03_101032) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_07_061904) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_03_101032) do
     t.float "lng", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address", null: false
     t.index ["lat", "lng"], name: "index_restaurants_on_lat_and_lng", unique: true
   end
 
