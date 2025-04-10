1fce315489a342475053cc8bae4618dc33633950
wisk-env <wiskenv566@gmail.com>
2025-04-10 14:06:24 +0900

    モデルの設定:ユーザーのデフォルトのプロフィール画像を設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,21 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  before_create :default_icon
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true
   attr_accessor :current_password
+  has_one_attached :avatar
   has_many :bookmarks, dependent: :destroy
   has_many :bookmark_restaurants, through: :bookmarks, source: :restaurant
+
+  def default_icon
+    if !self.avatar.attached?
+      self.avatar.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'default_icon.png')), filename: 'default_icon.png', content_type: 'image/png')
+    end
+  end
 
   def bookmark(restaurant)
     bookmark_restaurants << restaurant
