92a2f24e457ed17ae7196123b16bee4f9a7f5c27
wisk-env <wiskenv566@gmail.com>
2025-04-15 21:48:20 +0900

    Likesテーブルへのレコードの登録・削除を行うために必要なルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,16 +6,11 @@ Rails.application.routes.draw do
   }
   resource :profile, only: %i[show]
   resources :restaurants do
-    resources :reviews, only: %i[show new create]
+    resources :reviews, only: %i[show new create] do
+      resource :likes, only: %i[create destroy]
+    end
   end
   resources :restaurant_registered_statuses, only: %i[index show new]
   resources :restaurant_unregistered_statuses, only: %i[index]
   resources :bookmarks, only: %i[create destroy]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
