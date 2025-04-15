0af147e8c67c2b40603fec6926e08de18eba7ea5
wisk-env <wiskenv566@gmail.com>
2025-04-15 21:44:26 +0900

    Likeモデルの作成

---
+++ db/migrate/20250415043922_create_likes.rb
@@ -0,0 +1,11 @@
+class CreateLikes < ActiveRecord::Migration[7.1]
+  def change
+    create_table :likes do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :review, null: false, foreign_key: true
+
+      t.timestamps
+      t.index [:user_id, :review_id], unique: true
+    end
+  end
+end
