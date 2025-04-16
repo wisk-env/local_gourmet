efe99fbfea4c6c38fff4213de5622eff494b734e
wisk-env <wiskenv566@gmail.com>
2025-04-16 21:24:54 +0900

    ransackで検索可能な属性をモデルに設定

--- app/models/feedback_option.rb
+++ app/models/feedback_option.rb
@@ -1,4 +1,8 @@
 class FeedbackOption < ApplicationRecord
   has_many :review_feedback_options, dependent: :destroy
   has_many :reviews, through: :review_feedback_options
+  
+  def self.ransackable_attributes(auth_object = nil)
+    ["id", "option_title"]
+  end
 end
