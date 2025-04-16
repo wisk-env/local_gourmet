6004733330006ea83dade7cb5cb36e275aed1b55
wisk-env <wiskenv566@gmail.com>
2025-04-16 21:23:24 +0900

    reviewsコントローラーにアクション追加

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,12 @@
 class ReviewsController < ApplicationController
+  before_action :search_params, only: %i[index search]
+
+  def index
+    @reviews = Review.all
+    @genres = Genre.all
+    @feedback_options = FeedbackOption.all
+  end
+
   def show
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.find(params[:id])
@@ -21,9 +29,20 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def search
+    @reviews = Review.all
+    @genres = Genre.all
+    @feedback_options = FeedbackOption.all
+    @results = @q.result.distinct
+  end
+
   private
 
   def review_params
     params.require(:review).permit(:menu, :price, :visit_date, :visit_time, :number_of_visitors, :comment, :image, :user_id, :restaurant_id, feedback_option_ids: [], genre_ids: [])
+  end
+
+  def search_params
+    @q = Review.ransack(params[:q])
   end
 end
