70af4f781ae65ea0170efd19e37b1b04bb7f53ac
wisk-env <wiskenv566@gmail.com>
2025-04-17 16:15:20 +0900

    reviewsテーブルに関連づいているテーブルがあるためreviewsテーブルのレコード削除時にdependentオプションを使用

--- app/models/review.rb
+++ app/models/review.rb
@@ -7,9 +7,9 @@ class Review < ApplicationRecord
   has_one_attached :image
   belongs_to :user
   belongs_to :restaurant
-  has_many :review_feedback_options
+  has_many :review_feedback_options, dependent: :destroy
   has_many :feedback_options, through: :review_feedback_options
-  has_many :review_genres
+  has_many :review_genres, dependent: :destroy
   has_many :genres, through: :review_genres
   has_many :likes, dependent: :destroy
   has_many :users, through: :likes
