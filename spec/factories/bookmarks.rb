8bd3bf9d3e84e1f941523c629496cf3c65d83dd1
wisk-env <wiskenv566@gmail.com>
2025-04-21 18:01:15 +0900

    ユーザーのアソシエーションテストに必要なテストデータを作成

---
+++ spec/factories/bookmarks.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :bookmark do
+    sequence(:user_id) { |n| n }
+    sequence(:restaurant_id) { |n| n }
+    association :user
+    association :restaurant
+  end
+end
