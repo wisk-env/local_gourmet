8f5072e20e568c3f8976567191da96b5830ccbb9
wisk-env <wiskenv566@gmail.com>
2025-04-26 23:21:43 +0900

    ブックマークできる場合のテストを作成

---
+++ spec/models/bookmark_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe Bookmark, type: :model do
+  before do
+    @user = FactoryBot.create(:user)
+    @restaurant = FactoryBot.create(:restaurant)
+  end
+
+  describe 'ブックマーク機能' do
+    context 'ブックマークできる場合' do
+      it 'user_idとrestaurant_idがあればブックマークできる' do
+        bookmark = FactoryBot.build(:bookmark, user_id: @user.id, restaurant_id: @restaurant.id)
+        expect(bookmark).to be_valid
+      end
+
+      it 'user_idが同じでも、restaurant_idが異なればbookmarksテーブルに保存できる' do
+        another_restaurant = FactoryBot.create(:restaurant)
+        bookmark = FactoryBot.create(:bookmark, user_id: @user.id, restaurant_id: @restaurant.id)
+        expect(
+          FactoryBot.create(:bookmark,
+          user_id: @user.id, restaurant_id: another_restaurant.id)
+          ).to be_valid
+      end
+    end
+  end
+end
