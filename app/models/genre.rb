2bf8f0261c8338db83ed924a02c9f63c87807cb2
wisk-env <wiskenv566@gmail.com>
2025-04-30 00:36:54 +0900

    Genreモデルの単体テスト作成

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,5 +1,6 @@
 class Genre < ApplicationRecord
-  has_many :review_genres
+  validates :name, presence: true
+  has_many :review_genres, dependent: :destroy
   has_many :reviews, through: :review_genres
 
   def self.ransackable_attributes(auth_object = nil)
