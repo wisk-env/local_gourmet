7c31df6d51b7f2a60a77f7bd6faa8ef2a1a9e57d
wisk-env <wiskenv566@gmail.com>
2025-05-14 14:53:01 +0900

    MiniMagickの設定

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ module LocalFoodie
 
     config.i18n.default_locale = :ja
 
+    config.active_storage.variant_processor = :mini_magick
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
